d16ebd08af7f8b5f9b62ade18695a628a05e2b75
zhangjiang <896884553@q.com>
2017-01-07 22:34:21 +0800

    Initial Commit

---
+++ ZJCustomSegment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJCustomSegment
+//
+//  Created by pg on 2017/1/7.
+//  Copyright © 2017年 DZHFCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
